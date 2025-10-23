1ad130ece16e682057777f3a7c94dca40c77c939
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2025-10-23 23:53:10 +0500

    Подключил роутер, прописал статические ссылки

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <div>Vue 3</div>
+    <div class="navbar__btns">
+      <my-button>Посты</my-button>
+      <my-button>О сайте</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
